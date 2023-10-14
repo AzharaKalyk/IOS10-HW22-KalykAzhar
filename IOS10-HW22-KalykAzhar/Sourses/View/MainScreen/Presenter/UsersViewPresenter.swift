359acf085c59b6ec08f989f3f42a3434d293fca7
AzharaKalyk <kab_180495@mail.ru>
2023-10-14 23:24:29 +0600

    all_bag_fix

---
+++ IOS10-HW22-KalykAzhar/Sourses/View/MainScreen/Presenter/UsersViewPresenter.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+class Presenter: UserViewInput {
+    
+    weak var view: UserViewOutput?
+    let dataManager: CoreDataProtocol
+    
+    required init(view: UserViewOutput, dataManager: CoreDataProtocol) {
+        self.view = view
+        self.dataManager = dataManager
+    }
+    
+    func fetchUsers() {
+        dataManager.fetchUsers()
+        view?.updateUsers(dataManager.users ?? [])
+    }
+    
+    func addNewUser(name: String) {
+        dataManager.addNewUser(name: name)
+        view?.reloadData()
+    }
+    
+    func deleteUser(_ index: Int) {
+        dataManager.deleteUser(index: index)
+        view?.reloadData()
+    }
+    
+    func getModelDataCount() -> Int {
+        return dataManager.users?.count ?? 0
+    }
+    
+    func getContact(_ index: Int) -> User? {
+        return dataManager.users?[index]
+    }
+}
